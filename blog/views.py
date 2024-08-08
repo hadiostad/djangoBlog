34793eab1b7f61b21d5942d6ff36864a7379cb0d
hadiostad <hadi.abdollahzadeh81@gmail.com>
2024-08-08 12:44:29 +0330

    Config Author And Tags Data

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
-
-
-
 def main_page(request):
     latest_posts = Post.objects.all().order_by('-date')[:3]
     return render(request, 'blog/index.html', {
@@ -22,5 +19,6 @@ def posts(request):
 def single_post(request, slug):
     recognized_post = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/single-post.html', {
-        'post': recognized_post
+        "post": recognized_post,
+        "post_tags": recognized_post.tags.all()
     })
