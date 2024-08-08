1b77a90873031ec8c52ab38083194d0bb4dc1d54
hadiostad <hadi.abdollahzadeh81@gmail.com>
2024-08-08 11:54:57 +0330

    Adding Filter And List Display

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Post, Author, Tag
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date")
+    list_display = ("title", "author", "date")
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
