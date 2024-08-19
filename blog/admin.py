30bd2d8c1408d8dacf79b98ee71bfb2ab56b44d2
hadiostad <hadi.abdollahzadeh81@gmail.com>
2024-08-19 20:34:01 +0330

    Adding Description

--- blog/admin.py
+++ blog/admin.py
@@ -2,13 +2,19 @@ from django.contrib import admin
 from .models import Post, Author, Tag, Comment
 
 
+# Admin configuration for the Post model
 class PostAdmin(admin.ModelAdmin):
+    # filtering posts
     list_filter = ("author", "tags", "date")
+    # Specifies the fields to be displayed in the list view of posts
     list_display = ("title", "author", "date")
+    # Automatically populates the slug field based on the title when creating a new post.
     prepopulated_fields = {"slug": ["title"]}
 
 
+# Admin configuration for the Comment model.
 class CommentsAdmin(admin.ModelAdmin):
+    # Specifies the fields to be displayed in the list view of posts
     list_display = ("user_name", "post")
 
 
