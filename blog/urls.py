5a964b9b932e34fb54afd03e2e074c02d2f30b04
hadiostad <hadi.abdollahzadeh81@gmail.com>
2024-08-19 20:26:50 +0330

    Adding Description

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 
 urlpatterns = [
+    # Maps the root URL to the main page view.
     path('', views.Main_Page_View.as_view(), name='main-page'),
+    # Maps the posts list page to the PostListView.
     path('posts', views.PostListView.as_view(), name='posts-page'),
+    # Maps a post detail page to the PostDetailView, capturing the post slug.
     path('posts/<slug:slug>', views.PostDetailView.as_view(), name='single-post-page'),
+    # Maps the add favorite action to the AddFavouriteView.
     path('favourite', views.AddFavouriteView.as_view(), name="add-favourite")
 ]
