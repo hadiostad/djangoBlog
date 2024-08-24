1466cdd81263f7961c2c44971e6a55a038ed74e9
hadiostad <hadi.abdollahzadeh81@gmail.com>
2024-08-24 19:00:49 +0330

    Serving Static Files

--- djangoBlog/urls.py
+++ djangoBlog/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings  # import configuration
 
-urlpatterns = [
+urlpatterns = ([
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Serving Uploaded File
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) /  # Serving Uploaded File
+               + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))  # Serving Static File
